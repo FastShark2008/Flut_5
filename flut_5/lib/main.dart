8b35d363cdd874244da687632468127fed73e3ed
FastShark <fastshark2008@yandex.ru>
2019-10-30 16:55:51 +0400

    add image

--- flut_5/lib/main.dart
+++ flut_5/lib/main.dart
@@ -4,9 +4,38 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: FaceContact(),
+      ),
+  );
   }
 }
 
 // Верхний блок с изображением
+class FaceContact extends StatelessWidget {
 
+  Widget build(BuildContext context) {
+  return Container(child:
+    Image.network('https://i.pinimg.com/originals/5e/81/37/5e81371c890a88377dd2195a99d3deba.jpg',
+    fit: BoxFit.fitWidth
+    ),
+    padding: EdgeInsets.all(3.0),
+    width: double.infinity,
+    height: 350,
+    alignment: Alignment.topCenter
+    );
+  }
+}
+
+
+// Блок наименование контакта
+
+
+
+// Блок контактов
+
+
+
+// Блок навигации
