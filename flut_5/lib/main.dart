97ff40b6d7e67d42be77c25493f0b9f258ae3839
FastShark <fastshark2008@yandex.ru>
2019-10-31 16:13:20 +0400

    image ready

--- flut_5/lib/main.dart
+++ flut_5/lib/main.dart
@@ -19,12 +19,24 @@ class FaceContact extends StatelessWidget {
   Widget build(BuildContext context) {
   return Container(child:
     Image.network('https://i.pinimg.com/originals/5e/81/37/5e81371c890a88377dd2195a99d3deba.jpg',
-    fit: BoxFit.fitWidth
+    fit: BoxFit.cover,
+    //scale: 1.0,
     ),
-    padding: EdgeInsets.all(3.0),
+
+    //decoration: BoxDecoration(
+    //    boxShadow: [BoxShadow(
+    //      color: Colors.black45,
+    //      blurRadius: 5.0,
+    //      spreadRadius: 5.0,
+    //      offset: Offset(-5, 0)
+    //    )]
+    // ),
+
+    color: Colors.black,
+    padding: EdgeInsets.all(0.0),
     width: double.infinity,
     height: 350,
-    alignment: Alignment.topCenter
+    // alignment: Alignment.topCenter
     );
   }
 }
